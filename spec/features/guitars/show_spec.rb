4165bee7a9982ee469cb2eb7c287cba4c20e4521
James Harkins <jh.harkins@protonmail.com>
2022-03-23 15:57:40 -0600

    feat: add link to guitars#index page on all pages and pass feature tests

--- spec/features/guitars/show_spec.rb
+++ spec/features/guitars/show_spec.rb
@@ -31,10 +31,38 @@ RSpec.describe 'guitars show page' do
     expect(page).to have_content(guitar_2.six_string)
     expect(page).not_to have_content(guitar_1.model)
     expect(page).not_to have_content(guitar_3.num_of_frets)
+  end
+
+  it 'has a redirect link to the guitars index page' do
+    manufacturer_1 = Manufacturer.create!(name: "Fender",
+                                         headquarter_city: "Los Angeles",
+                                         years_in_business: 76,
+                                         custom_shop: true,
+                                        )
+    manufacturer_2 = Manufacturer.create!(name: "Gibson",
+                                         headquarter_city: "Nashville",
+                                         years_in_business: 120,
+                                         custom_shop: true,
+                                        )
+    guitar_1 = manufacturer_1.guitars.create!(model: 'Stratocaster',
+                                              num_of_frets: 21,
+                                              six_string: true
+                                             )
+    guitar_2 = manufacturer_1.guitars.create!(model: 'Telecaster',
+                                              num_of_frets: 21,
+                                              six_string: true
+                                             )
+    guitar_3 = manufacturer_2.guitars.create!(model: 'Les Paul',
+                                              num_of_frets: 22,
+                                              six_string: true
+                                             )
+
+    visit "/manufacturers/#{manufacturer_1.id}"
+
     expect(page).to have_link("Guitars Index")
 
     click_link "Guitars Index"
 
-    expect(page).to eq("/guitars")
+    expect(current_path).to eq("/guitars")
   end
 end
