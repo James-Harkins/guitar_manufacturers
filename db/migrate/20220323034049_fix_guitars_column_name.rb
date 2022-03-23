2c000f5ce7f75ae674d068b0b61006615fc4422a
James Harkins <jh.harkins@protonmail.com>
2022-03-23 15:12:01 -0600

    test: create feature test for sorting manufacturer index by created_at

--- db/migrate/20220323034049_fix_guitars_column_name.rb
+++
@@ -1,5 +0,0 @@
-class FixGuitarsColumnName < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :guitars, :model_name, :model
-  end
-end
--- db/migrate/20220323040418_add_manufacturer_to_guitars.rb
+++
@@ -1,5 +0,0 @@
-class AddManufacturerToGuitars < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :guitars, :manufacturer, foreign_key: true
-  end
-end
