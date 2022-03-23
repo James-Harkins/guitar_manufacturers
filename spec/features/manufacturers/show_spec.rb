3cc2189fbe8d5a730055e0c411699aa622f54680
James Harkins <jh.harkins@protonmail.com>
2022-03-23 15:35:03 -0600

    test: create feature test for manufacturer#show page to show guitars count

--- spec/features/manufacturers/show_spec.rb
+++ spec/features/manufacturers/show_spec.rb
@@ -19,4 +19,33 @@ RSpec.describe 'manufacturers show page', type: :feature do
     expect(page).to have_content(manufacturer_1.custom_shop)
     expect(page).not_to have_content(manufacturer_2.name)
   end
+
+  it 'lists the number of guitars associated with the manufacturer' do
+    manufacturer_1 = Manufacturer.create!(name: "Fender",
+                                         headquarter_city: "Los Angeles",
+                                         years_in_business: 76,
+                                         custom_shop: true,
+                                        )
+    manufacturer_2 = Manufacturer.create!(name: "Gibson",
+                                         headquarter_city: "Nashville",
+                                         years_in_business: 120,
+                                         custom_shop: true,
+                                        )
+    guitar_1 = manufacturer_1.guitars.create!(model: 'Stratocaster',
+                                              num_of_frets: 21,
+                                              six_string: true
+                                             )
+    guitar_2 = manufacturer_1.guitars.create!(model: 'Telecaster',
+                                              num_of_frets: 21,
+                                              six_string: true
+                                             )
+    guitar_3 = manufacturer_2.guitars.create!(model: 'Les Paul',
+                                              num_of_frets: 22,
+                                              six_string: true
+                                             )
+
+    visit "/manufacturers/#{manufacturer_1.id}"
+
+    expect(page).to have_content("Current model count: #{manufacturer_1.guitars.count}")
+  end
 end
