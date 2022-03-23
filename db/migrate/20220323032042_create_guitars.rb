d2da8bc8e9007b6c764572a815ef6c60d57abc92
James Harkins <jh.harkins@protonmail.com>
2022-03-22 21:22:05 -0600

    feat: create new table for guitars

--- db/migrate/20220323032042_create_guitars.rb
+++ db/migrate/20220323032042_create_guitars.rb
@@ -4,7 +4,6 @@ class CreateGuitars < ActiveRecord::Migration[5.2]
       t.string :model_name
       t.integer :num_of_frets
       t.boolean :six_string
-      t.integer :manufacturer_id
       t.timestamps
     end
   end
