061b862f9d3778b550a4365e21dba7929f93e1f2
James Harkins <jh.harkins@protonmail.com>
2022-03-22 22:08:54 -0600

    feat: create one to many association from manufacturers to guitars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_034049) do
+ActiveRecord::Schema.define(version: 2022_03_23_040418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2022_03_23_034049) do
     t.boolean "six_string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "manufacturer_id"
+    t.index ["manufacturer_id"], name: "index_guitars_on_manufacturer_id"
   end
 
   create_table "manufacturers", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2022_03_23_034049) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "guitars", "manufacturers"
 end
